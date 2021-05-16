542646a2cacc9ca599c255105db3bb00aa650994
gutomezencio <gutomezencio@gmail.com>
2021-05-16 18:47:10 -0300

    Add children elements for Carousel component.

--- src/app/components/Carousel/Carousel.jsx
+++ src/app/components/Carousel/Carousel.jsx
@@ -3,146 +3,176 @@ import PropTypes from 'prop-types'
 
 import './Carousel.scoped.scss'
 
-const Carousel = ({ visilbleItems, infinity }) => {
-  const wrapperRef = useRef()
-  const listRef = useRef()
-  const [currentSlide, setCurrentSlide] = useState(0)
-  const [slideCount, setSlideCount] = useState(0)
-  const [slideItemsEl, setSlideItemsEl] = useState(null)
+const Carousel = ({ children, visilbleItems, infinity, restartOnEnd }) => {
+  const wrapperRef = useRef();
+  const listRef = useRef();
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [slideCount, setSlideCount] = useState(0);
+  const [slideItemsEl, setSlideItemsEl] = useState(null);
+  const [childrenItems, setChildrenItems] = useState(null)
 
   const prevHandler = event => {
     if (currentSlide > 0) {
-      const slideTranslate = (currentSlide - 1) * 100
-      listRef.current.style.transform = `translate3d(-${slideTranslate}%, 0, 0)`
+      const slideTranslate = (currentSlide - 1) * 100;
+      listRef.current.style.transform = `translate3d(-${slideTranslate}%, 0, 0)`;
 
-      setCurrentSlide(currentSlide - 1)
-    } else if (infinity) {
-      if (!visilbleItems) {
-        const itemWidth = getAbsoluteWidth(slideItemsEl[0])
-        const cloneQuant = slideItemsEl.length
-        const leftMultiplier = currentSlide ? Math.abs(currentSlide) + 1 : 1
+      setCurrentSlide(currentSlide - 1);
+    } else if (infinity && !restartOnEnd) {
+      const itemWidth = getAbsoluteWidth(slideItemsEl[0]);
+      const cloneQuant = slideItemsEl.length;
+      const leftMultiplier = currentSlide ? Math.abs(currentSlide) + 1 : 1;
 
-        listRef.current.style.left = `-${((itemWidth.fullWidth + itemWidth.margin) * leftMultiplier) * cloneQuant}px`;
+      listRef.current.style.left = `-${(itemWidth.fullWidth +
+        itemWidth.margin) *
+        leftMultiplier *
+        cloneQuant}px`;
 
-        Array.from(slideItemsEl)
-          .reverse()
-          .forEach(elNode => {
-            listRef.current.insertBefore(
-              elNode.cloneNode(true),
-              listRef.current.querySelectorAll('.carousel__item')[0]
-            );
-          });
+      Array.from(slideItemsEl)
+        .reverse()
+        .forEach(elNode => {
+          listRef.current.insertBefore(
+            elNode.cloneNode(true),
+            listRef.current.querySelectorAll(".carousel__item")[0]
+          );
+        });
 
-        listRef.current.style.transform = `translate3d(${leftMultiplier * 100}%, 0, 0)`
-        setCurrentSlide(currentSlide - 1);
-      } else {
-        const slideTranslate = slideCount * 100
-        listRef.current.style.transform = `translate3d(-${slideTranslate}%, 0, 0)`
+      listRef.current.style.transform = `translate3d(${leftMultiplier *
+        100}%, 0, 0)`;
+      setCurrentSlide(currentSlide - 1);
+    } else if (restartOnEnd) {
+      const slideTranslate = slideCount * 100;
+      listRef.current.style.transform = `translate3d(-${slideTranslate}%, 0, 0)`;
 
-        setCurrentSlide(slideCount)
-      }
+      setCurrentSlide(slideCount);
     }
-  }
+  };
 
   const nextHandler = event => {
-    if (infinity && !visilbleItems) {
-      const itemWidth = getAbsoluteWidth(slideItemsEl[0])
-      const wrapperWidth = getAbsoluteWidth(wrapperRef.current)
-      const currentItemsEls = listRef.current.querySelectorAll('.carousel__item')
-      const listWidth = ((itemWidth.fullWidth * currentItemsEls.length) - itemWidth.margin) - listRef.current.style.left.replace(/px|-/g, '')
-      const currentCount = parseInt(listWidth / wrapperWidth.fullWidth)
-      const translationMultiplier = currentSlide < 0 ? Math.abs(currentSlide) - 1 : currentSlide + 1
+    if (infinity && !restartOnEnd) {
+      const itemWidth = getAbsoluteWidth(slideItemsEl[0]);
+      const wrapperWidth = getAbsoluteWidth(wrapperRef.current);
+      const currentItemsEls = listRef.current.querySelectorAll(
+        ".carousel__item"
+      );
+      const listWidth =
+        itemWidth.fullWidth * currentItemsEls.length -
+        itemWidth.margin -
+        listRef.current.style.left.replace(/px|-/g, "");
+      const currentCount = parseInt(listWidth / wrapperWidth.fullWidth);
+      const translationMultiplier =
+        currentSlide < 0 ? Math.abs(currentSlide) - 1 : currentSlide + 1;
 
       if (currentCount === currentSlide + 1) {
-        Array.from(slideItemsEl)
-          .forEach(elNode => {
-            const currentEls = listRef.current.querySelectorAll('.carousel__item')
+        Array.from(slideItemsEl).forEach(elNode => {
+          const currentEls = listRef.current.querySelectorAll(
+            ".carousel__item"
+          );
 
-            listRef.current.insertBefore(
-              elNode.cloneNode(true),
-              currentEls[currentEls.length - 1].nextSibling
-            )
-          })
+          listRef.current.insertBefore(
+            elNode.cloneNode(true),
+            currentEls[currentEls.length - 1].nextSibling
+          );
+        });
       }
-      listRef.current.style.transform = `translate3d(${currentSlide < 0 ? '' : '-'}${translationMultiplier * 100}%, 0, 0)`
-      setCurrentSlide(currentSlide + 1)
-    } else {
-      if (currentSlide < slideCount) {
-        const slideTranslate = (currentSlide + 1) * 100
-        listRef.current.style.transform = `translate3d(-${slideTranslate}%, 0, 0)`
+      listRef.current.style.transform = `translate3d(${
+        currentSlide < 0 ? "" : "-"
+      }${translationMultiplier * 100}%, 0, 0)`;
+      setCurrentSlide(currentSlide + 1);
+    } else if (currentSlide < slideCount) {
+      const slideTranslate = (currentSlide + 1) * 100;
+      listRef.current.style.transform = `translate3d(-${slideTranslate}%, 0, 0)`;
 
-        setCurrentSlide(currentSlide + 1)
-      } else if (infinity) {
-        listRef.current.style.transform = `translate3d(0, 0, 0)`
+      setCurrentSlide(currentSlide + 1);
+    } else if (restartOnEnd) {
+      listRef.current.style.transform = `translate3d(0, 0, 0)`;
 
-        setCurrentSlide(0)
-      }
+      setCurrentSlide(0);
     }
-  }
+  };
 
-  const getAbsoluteWidth = (el) => {
-    const margin = parseInt(getComputedStyle(el)['margin-right'].replace('px', ''))
-    const elementValues = el.getBoundingClientRect()
+  const getAbsoluteWidth = el => {
+    const margin = parseInt(
+      getComputedStyle(el)["margin-right"].replace("px", "")
+    );
+    const elementValues = el.getBoundingClientRect();
 
     return {
       fullWidth: elementValues.width,
-      margin,
+      margin
     };
-  }
+  };
 
   useEffect(() => {
-    let wrapperWidth = null;
-    let listWidth = null;
+    const itemsEls = listRef.current.querySelectorAll('.carousel__item')
 
-    if (wrapperRef?.current) {
-      wrapperWidth = getAbsoluteWidth(wrapperRef.current)
-    }
+    if (childrenItems && itemsEls.length) {
+      let wrapperWidth = null;
+      let listWidth = null;
 
-    if (listRef?.current) {
-      const itemsEls = listRef.current.querySelectorAll('.carousel__item')
-      let count = 0;
-
-      if (!visilbleItems) {
-        const elWidth = getAbsoluteWidth(itemsEls[0]);
-
-        listWidth = elWidth.fullWidth * itemsEls.length - elWidth.margin
-        count = parseInt(listWidth / wrapperWidth.fullWidth)
-      } else {
-        count = parseInt(itemsEls.length / visilbleItems)
-        count = itemsEls.length % visilbleItems === 0 ? count - 1 : count
+      if (wrapperRef?.current) {
+        wrapperWidth = getAbsoluteWidth(wrapperRef.current);
       }
 
-      setSlideCount(count);
-      setSlideItemsEl(itemsEls);
+      if (listRef?.current) {
+        let count = 0;
+
+        if (!visilbleItems) {
+          const elWidth = getAbsoluteWidth(itemsEls[0]);
+
+          listWidth = elWidth.fullWidth * itemsEls.length - elWidth.margin;
+          count = parseInt(listWidth / wrapperWidth.fullWidth);
+        } else {
+          count = parseInt(itemsEls.length / visilbleItems);
+          count = itemsEls.length % visilbleItems === 0 ? count - 1 : count;
+        }
+
+        setSlideCount(count);
+        setSlideItemsEl(itemsEls);
+      }
     }
-  }, [wrapperRef, listRef, visilbleItems]);
+  }, [wrapperRef, listRef, visilbleItems, childrenItems])
 
   useEffect(() => {
-    if (visilbleItems) {
-      listRef.current.querySelectorAll('.carousel__item').forEach((item) => {
-        item.style.minWidth = `${100 / visilbleItems}%`
-        item.style.marginRight = '0'
-      })
-    }
-  }, [visilbleItems])
+    if (children) {
+      const processedItems = children.map((item, index) => {
+        return React.cloneElement(item, {
+          key: index,
+          className: item.props.className + " carousel__item",
+          style: {
+            ...item.style,
+            ...(visilbleItems ? {
+              minWidth: `${100 / visilbleItems}%`,
+              marginRight: '0'
+            } : {})
+          }
+        });
+      });
 
-  const buttonInactive = useCallback((type) => {
-    if(!infinity) {
-      if (type === 'next') {
-        return currentSlide === slideCount
-      } else if (type === 'prev') {
-        return currentSlide === 0
+      setChildrenItems(processedItems)
+    }
+  }, [children, setChildrenItems]);
+
+  const buttonInactive = useCallback(
+    type => {
+      if (!infinity && !restartOnEnd) {
+        if (type === "next") {
+          return currentSlide === slideCount;
+        } else if (type === "prev") {
+          return currentSlide === 0;
+        }
       }
-    }
 
-    return false
-  }, [currentSlide, slideCount, infinity])
+      return false;
+    },
+    [currentSlide, slideCount, infinity, restartOnEnd]
+  );
 
   return (
     <div className="carousel">
       <div className="carousel__wrapper" ref={wrapperRef}>
         <div className="carousel__list" ref={listRef}>
-          <div className="carousel__item">
+          {childrenItems}
+          {/* <div className="carousel__item">
             <img src="https://picsum.photos/id/11/200/300" alt="Some image" />
           </div>
 
@@ -174,7 +204,7 @@ const Carousel = ({ visilbleItems, infinity }) => {
             <img src="https://picsum.photos/id/88/200/300" alt="Some image" />
           </div>
 
-          {/* <div className="carousel__item">
+          <div className="carousel__item">
             <img src="https://picsum.photos/id/99/200/300" alt="Some image" />
           </div> */}
         </div>
@@ -208,11 +238,12 @@ const Carousel = ({ visilbleItems, infinity }) => {
       </div>
     </div>
   );
-}
+};
 
 Carousel.propTypes = {
   visilbleItems: PropTypes.number,
-  infinity: PropTypes.bool
+  infinity: PropTypes.bool,
+  restartOnEnd: PropTypes.bool
 };
 
 export default Carousel
--- src/app/modules/CarouselArea.jsx
+++ src/app/modules/CarouselArea.jsx
@@ -3,26 +3,69 @@ import React from 'react'
 import Carousel from 'app/components/Carousel/Carousel'
 
 const CarouselArea = () => {
+  const carouselItems = [
+    'https://picsum.photos/id/111/300/300',
+    'https://picsum.photos/id/122/300/300',
+    'https://picsum.photos/id/133/300/300',
+    'https://picsum.photos/id/144/300/300',
+    'https://picsum.photos/id/155/300/300',
+    'https://picsum.photos/id/166/300/300',
+    'https://picsum.photos/id/177/300/300',
+    'https://picsum.photos/id/188/300/300',
+    'https://picsum.photos/id/199/300/300'
+  ]
+
+  const generateItems = quant => {
+    return carouselItems?.slice(0, quant)?.map((image, index) => {
+      return (
+        <div className="some-class" key={index}>
+          <img src={image} alt={`image-${index}`} />
+        </div>
+      );
+    })
+  }
   return (
     <div className="carousel-area">
       <div className="carousel-area__item">
-        <Carousel />
+        <Carousel>
+          {generateItems(9)}
+        </Carousel>
       </div>
 
       <div className="carousel-area__item">
-        <Carousel visilbleItems={1} />
+        <Carousel infinity={true}>
+          {generateItems(4)}
+        </Carousel>
       </div>
 
       <div className="carousel-area__item">
-        <Carousel infinity={true} />
+        <Carousel visilbleItems={5} infinity={true}>
+          {generateItems(8)}
+        </Carousel>
       </div>
 
       <div className="carousel-area__item">
-        <Carousel visilbleItems={5} infinity={true} />
+        <Carousel infinity={true}>
+          {generateItems(8)}
+        </Carousel>
       </div>
 
       <div className="carousel-area__item">
-        <Carousel visilbleItems={2} infinity={true} />
+        <Carousel visilbleItems={1} restartOnEnd={true}>
+          {generateItems(9)}
+        </Carousel>
+      </div>
+
+      <div className="carousel-area__item">
+        <Carousel visilbleItems={2} infinity={true}>
+          {generateItems(5)}
+        </Carousel>
+      </div>
+
+      <div className="carousel-area__item">
+        <Carousel visilbleItems={1} restartOnEnd={true}>
+          {generateItems(9)}
+        </Carousel>
       </div>
     </div>
   );
